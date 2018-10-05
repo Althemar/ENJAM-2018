ae193f69eff56c30100d1f79aa3e66e1e305309f
Althemar <damien.crechet@gmail.com>
2018-10-05 23:43:26 +0200

    Basic player controller

---
+++ ENJAM-2018/Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    Vector3 dashTarget;
+    float dashProgress;
+
+    CharacterState characterState = CharacterState.Waiting;
+    PlayersManager playerController;
+
+    enum CharacterState
+    {
+        Waiting,
+        MovingForward
+    }
+
+    private void Start() {
+        playerController = GetComponentInParent<PlayersManager>();
+    }
+
+    void Update () {
+
+        switch (characterState) {
+
+            case CharacterState.Waiting :
+                transform.position += Vector3.left * playerController.MovingBackSpeed * Time.deltaTime;
+                break;
+
+            case CharacterState.MovingForward:
+
+                dashProgress += playerController.DashSpeed * Time.deltaTime;
+                transform.position = Vector3.Lerp(transform.position, dashTarget, dashProgress);
+
+                if (dashProgress >= 1) {
+                    characterState = CharacterState.Waiting;
+                    dashProgress = 0;
+                }
+                break;
+        } 
+    }
+
+    public void MoveForward() {
+        dashProgress = 0;
+        dashTarget = transform.position + new Vector3(playerController.DashDistance, 0, 0);
+        characterState = CharacterState.MovingForward;
+    }
+}
---
+++ ENJAM-2018/Assets/Scripts/PlayersManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayersManager : MonoBehaviour {
+
+    public float MovingBackSpeed;
+    public float DashSpeed;
+    public float DashDistance;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
