fc6e10970f5021e8549526b824da0baba460837e
Xenation <xenations.111@gmail.com>
2018-10-06 11:02:01 +0200

    Tile Sequence WIP

---
+++ ENJAM-2018/Assets/Scripts/LevelSequence.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ENJAM2018 {
+	public class LevelSequence : MonoBehaviour {
+
+		[SerializeField] private Camera camera;
+		[SerializeField] private float tileSize = 2f;
+		[SerializeField] private float speed = 2f;
+		[SerializeField] private List<SequenceInputType> debugInputs;
+		[Header("Temporary")]
+		[SerializeField] private GameObject prefabUp;
+		[SerializeField] private GameObject prefabRight;
+		[SerializeField] private GameObject prefabDown;
+		[SerializeField] private GameObject prefabLeft;
+		
+		private SequenceTile[] tiles;
+
+		private void Awake() {
+			float vertExtent = camera.orthographicSize;
+			float horizExtent = vertExtent * (Screen.width / Screen.height);
+			tiles = new SequenceTile[(int) (horizExtent / tileSize) + 1];
+			SequenceInput.Init(prefabUp, prefabRight, prefabDown, prefabLeft);
+		}
+
+		private void Start() {
+			GenerateSequence(debugInputs);
+		}
+
+		private void FixedUpdate() {
+			transform.position -= Vector3.right * speed * Time.fixedDeltaTime;
+		}
+
+		private void GenerateSequence(List<SequenceInputType> inputs) {
+			for (int i = 0; i < inputs.Count; i++) {
+				GameObject go = Instantiate(inputs[i].GetSequenceInput().prefab, transform);
+				SequenceTile tile = go.GetComponent<SequenceTile>();
+				tile.requiredInput = inputs[i].GetSequenceInput();
+				go.transform.position = new Vector3(tileSize * i, 0f);
+			}
+		}
+
+	}
+}
---
+++ ENJAM-2018/Assets/Scripts/SequenceInput.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace ENJAM2018 {
+	public enum SequenceInputType {
+		Up,
+		Right,
+		Down,
+		Left
+	}
+	public static class SequenceInputTypeExt {
+		public static SequenceInput GetSequenceInput(this SequenceInputType type) {
+			switch (type) {
+				case SequenceInputType.Up:
+					return SequenceInput.Up;
+				case SequenceInputType.Right:
+					return SequenceInput.Right;
+				case SequenceInputType.Down:
+					return SequenceInput.Down;
+				case SequenceInputType.Left:
+					return SequenceInput.Left;
+			}
+			return null;
+		}
+	}
+	public class SequenceInput {
+		
+		public static SequenceInput Up;
+		public static SequenceInput Right;
+		public static SequenceInput Down;
+		public static SequenceInput Left;
+
+		public static void Init(GameObject prefabUp, GameObject prefabRight, GameObject prefabDown, GameObject prefabLeft) {
+			Up = new SequenceInput(prefabUp);
+			Right = new SequenceInput(prefabRight);
+			Down = new SequenceInput(prefabDown);
+			Left = new SequenceInput(prefabLeft);
+		}
+
+		public GameObject prefab;
+		public int inputKey;
+
+		public SequenceInput(GameObject prefab) {
+			this.prefab = prefab;
+		}
+
+	}
+}
